fb48058e50bb140c2e8a3a4eb05b2e409577082d
Adam-McNeil <adam.mcneil@oasisdigital.com>
2023-04-14 20:52:25 -0500

    fox and rabbits have random movement

--- Assets/Prefabs/HopRandomDirection.cs
+++ Assets/Prefabs/HopRandomDirection.cs
@@ -6,28 +6,26 @@ public class HopRandomDirection : MonoBehaviour
 {
     public float hopForce = 5f; // The force applied to the hop
 
-    private Rigidbody rb; // Reference to the Rigidbody component
+    private Rigidbody rigidbody; // Reference to the Rigidbody component
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); // Get the Rigidbody component
+        rigidbody = GetComponent<Rigidbody>(); // Get the Rigidbody component
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        if (rb.velocity == new Vector3(0,0,0))
+        if (rigidbody.velocity == Vector3.zero)
         {
-            this.transform.RotateAround(Vector3.up, Random.Range(0f, 360f));
+            this.transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
             Hop();
         }
     }
 
     void Hop()
     {
-        Vector3 direction = this.transform.forward;
-        direction.y = 1;
-        Vector3 hopDirection = direction; // Generate a random direction vector with y set to 0
-        Vector3 hopForceVector = hopDirection * hopForce; // Scale the direction vector with hop force
-        rb.AddForce(hopForceVector, ForceMode.Impulse); // Apply the hop force as an impulse to the Rigidbody component
+        Vector3 direction = this.transform.forward + Vector3.up * 0.5f;
+        Vector3 hopForceVector = direction * hopForce; 
+        rigidbody.AddForce(hopForceVector, ForceMode.Impulse); 
     }
 }
